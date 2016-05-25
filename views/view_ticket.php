cc807dc482f1144bdf28252374bced5985ffc1cf
Arkadynn <quentindeputier@gmail.com>
2016-05-25 12:56:04 +0200

    ticket view

---
+++ views/view_ticket.php
@@ -0,0 +1,19 @@
+<table>
+	<tr>
+		<td colspan="2" class="titre">Titre</td>
+	</tr>
+	<tr>
+		<td class="objet">Objet</td>
+		<td class="importance">Importance</td>
+	</tr>
+	<tr>
+		<td colspan="2" class="etat">Etat</td>
+	</tr>
+	<tr>
+		<td colspan="2" class="corps">Corps</td>
+	</tr>
+	<tr>
+		<td class="tempsPref">Temps prefere</td>
+		<td class="user">Auteur</td>
+	</tr>
+</table>
